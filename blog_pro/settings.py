54541ce0a09fddb835403590ac790b5c347d1342
Ragul116 <ragulranjith1106@gmail.com>
2025-09-17 08:18:08 +0530

    initial commit

--- blog_pro/settings.py
+++ blog_pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@2j2svh0g3*@okzgsq@sc5!f9m1t@9=a4sndoy4o1)cgn(hikf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
